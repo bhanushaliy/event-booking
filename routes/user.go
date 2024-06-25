399a63cfdea4618e3e17ec0f0fca5ca6447f5687
bhanushaliy <bhanushaliyash18@gmail.com>
2024-06-25 11:09:09 -0700

    add create user api

--- routes/user.go
+++ routes/user.go
@@ -1,11 +1,25 @@
 package routes
 
 import (
+	"net/http"
+
 	"example.com/rest-api/models"
 	"github.com/gin-gonic/gin"
 )
 
 func createUser(context *gin.Context) {
 	var user models.Users
-	context.ShouldBindJSON(&user)
+	err := context.ShouldBindJSON(&user)
+	if err != nil{
+		context.JSON(http.StatusBadRequest, gin.H{"message":"Could not parse request data."})
+		return
+	}
+	user.ID = 1
+	err = user.Save()
+	if err != nil{
+		context.JSON(http.StatusInternalServerError, gin.H{"message":"Could not create user."})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"message":"User created!"})
+
 }
